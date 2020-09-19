74b1e2cdad30c8a219ad36c11759e3ef451eeaa9
developerfab <fab7696550@gmail.com>
2020-09-19 11:36:40 -0500

    Add nested order product to order model
    It includes the configuration to nested parameters to order product from
    order model, it also includes test and fixtures.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   validates :client_id, :total, :status, presence: true
 
   has_many :order_products
+
+  accepts_nested_attributes_for :order_products
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,20 +7,33 @@ RSpec.describe Order, type: :model do
   it { is_expected.to have_many(:order_products) }
 
   describe '#create' do
+    let(:parameters) do
+      {
+        client_id: 23,
+        total: 45000,
+        status: 'open',
+        comments: 'This client is important.'
+      }
+    end
+
+    subject { described_class.new(parameters) }
+
     context 'when the parameters are valid' do
-      let(:parameters) do
-        {
-          client_id: 23,
-          total: 45000,
-          status: 'open',
-          comments: 'This client is important.'
-        }
+      it 'saves the order' do
+        expect(subject.save).to eq true
+      end
+    end
+
+    context 'when creates a order_product also' do
+      let(:order_product) { JSON.parse(order_products(:luigis_restaurant).dup.to_json) }
+
+      before do
+        subject.order_products.build(order_product)
       end
 
-      let(:order) { described_class.new(parameters) }
-
-      it 'saves the order' do
-        expect(order.save).to eq true
+      it 'saves both objects' do
+        expect{ subject.save }.to change(Order, :count).by(1)
+          .and change(OrderProduct, :count).by(1)
       end
     end
   end
