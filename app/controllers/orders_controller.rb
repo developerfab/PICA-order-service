c9b9b3cf99a962c5adadc7ae8ba47267a6a6ccdf
developerfab <fab7696550@gmail.com>
2020-09-21 16:23:38 -0500

    Add show endpoint for orders
    It includes the endpoint and test to show information about an order.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,19 @@ class OrdersController < ApplicationController
     orders = Order.all
     render json: orders
   end
+
+  def show
+    order = Order.find(order_id)
+    if order
+      render json: order
+    else
+      render json: { error: order.errors }
+    end
+  end
+
+  private
+
+  def order_id
+    params.require(:id)
+  end
 end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe OrdersController, type: :controller do
       expect(JSON.parse(response.body)).to eq(expected_response)
     end
   end
+
+  describe '#show' do
+    context 'when the parameters are valid' do
+      let(:my_order){ orders(:travel_to_europe) }
+      let(:expected_response) { JSON.parse(my_order.to_json) }
+
+      before do
+        get :show, params: { id: my_order.id }
+      end
+
+      it 'returns a list of orders' do
+        expect(JSON.parse(response.body)).to eq(expected_response)
+      end
+    end
+  end
 end
